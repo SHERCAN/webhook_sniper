d74db1f8f1ceca3a10f482e71f36dfa6fac32752
SHERCAN <sidereoyess@gmail.com>
2022-04-22 11:25:40 -0500

    cm0.4

--- app.py
+++ app.py
@@ -2,6 +2,7 @@
 from threading import Thread
 from json import load
 from time import sleep
+from requests import post
 from binance.client import Client
 from flask import Flask, request, render_template
 # --------------------
@@ -24,10 +25,21 @@ class Cliente:
 # ordenes de compra y venta
 
 
+class Mensaje:
+
+    def __init__(self):
+        pass
+
+    def send(self, data: str):
+        post('https://api.telegram.org/bot5243749301:AAHIDCwt13NLYpmJ7WVaJLs57G0Z_IyFTLE/sendMessage',
+             data={'chat_id': '-548326689', 'text': data})
+
+
 class Ordenes:
 
     def __init__(self, symbol: str) -> None:
         self.cliente = Cliente(symbol.replace('PERP', ''))
+        self.message = Mensaje()
 
     def create_order(self, position: str):
         balance = self.cliente.client.futures_account_balance()
@@ -40,22 +52,20 @@ class Ordenes:
         except:
             pass
         print("Llego a crear la orden de compra en market")
-        """ order = self.cliente.client.futures_create_order(
+        order = self.cliente.client.futures_create_order(
             symbol=self.cliente.base['symbol'],
             side=position,
             type='MARKET',
             quantity=str((balance*30)/float(
                 self.cliente.client.futures_symbol_ticker(
                     symbol=self.cliente.base['symbol'])['price']))[0:5]
-        ) 
+        )
         self.cliente.base['side'] = position
         self.cliente.base['quantity'] = order['origQty']
         self.cliente.base['id'] = order['orderId']
-        # print(datos)"""
-        # order_exe = Thread(target=self.create_order_exe,
-        #                   args=(self.cliente.base['id'], 'create',))
+        # print(datos)
         order_exe = Thread(target=self.create_order_exe,
-                           args=(5354530132, 'create',))
+                           args=(self.cliente.base['id'], 'create',))
         order_exe.start()
         # print(order)
 
@@ -87,6 +97,7 @@ class Ordenes:
         order_exe = Thread(target=self.create_order_exe,
                            args=(stop['orderId'], 'stop',))
         order_exe.start()
+        return price
 
     def take_profit(self, position: str):
         if position == 'BUY':
@@ -114,18 +125,20 @@ class Ordenes:
         order_exe = Thread(target=self.create_order_exe,
                            args=(take['orderId'], 'take',))
         order_exe.start()
+        return price
 
     def create_order_exe(self, order_id, intro: str):
         while True:
             order = self.cliente.client.futures_get_order(
                 orderId=order_id, symbol=self.cliente.base['symbol'])
             print(intro, order['status'], order['orderId'])
-            if ((order['status'] == 'FILLED' and intro == 'create') or
-                    intro == 'create' and order['status'] == 'CANCELED'):
+            if (order['status'] == 'FILLED' and intro == 'create'):
                 self.cliente.base['price'] = order['avgPrice']
                 print('Entro a crear las ordenes de stop y take')
-                #self.stop_loss(self.cliente.base['side'])
-                #self.take_profit(self.cliente.base['side'])
+                price_stop = self.stop_loss(self.cliente.base['side'])
+                price_take = self.take_profit(self.cliente.base['side'])
+                self.message.send('El precio de compra fue '+str(self.cliente.base["price"])+' el precio del stop es '+str(
+                    price_stop)+' y el precio del take es de '+str(price_take))
                 break
             if ((order['status'] == 'FILLED' and intro == 'stop') or
                     intro == 'stop' and order['status'] == 'CANCELED'):
@@ -137,7 +150,7 @@ class Ordenes:
                 self.cliente.client.futures_cancel_all_open_orders(
                     self.cliente.base['symbol'])
                 break
-            sleep(2)
+            sleep(1)
 
     def prueba(self):
         print('Entro bien')
@@ -161,16 +174,15 @@ if __name__ == "__main__":
                 print('long')
                 orders = Ordenes(recive['ticker'])
                 orders.create_order('BUY')
-                orders.prueba()
                 mensaje = 'Se realizo una orden en long'
             elif recive['position'] == '-1' and recive['order'] == 'sell':
                 print('short')
+                orders = Ordenes(recive['ticker'])
                 orders.create_order('SELL')
-                orders.prueba()
                 mensaje = 'Se realizo una orden en short'
             return mensaje
-    app.run(host='127.0.0.1', port=80)
-    #app.run(host='0.0.0.0', port=80)
+    #app.run(host='127.0.0.1', port=80)
+    app.run(host='0.0.0.0', port=80)
 # ----------json recepción
 '''
 Envio del dato desde el cliente, de esta manera
